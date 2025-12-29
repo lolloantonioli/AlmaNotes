2d22cc8d83ef250aa007bacaf2b48c6601c30720
Lorenzo Antonioli <lorenzo.antonioli2@studio.unibo.it>
2025-12-29 11:22:19 +0100

    Login to access the site

--- php/bootstrap.php
+++ php/bootstrap.php
@@ -1,7 +1,13 @@
 <?php
 session_start();
-define("UPLOAD_DIR", "./upload/");
+define("UPLOAD_DIR", "./uploads/");
 require_once("utils/functions.php");
 require_once("../db/database.php");
 $dbh = new Database("127.0.0.1", "root", "", "almanotes", "3306");
+$paginaCorrente = basename($_SERVER['PHP_SELF']);
+$paginePubbliche = ['login.php', 'signup.php', 'login-form.php', 'signup-form.php'];
+if (!isUserLoggedIn() && !in_array($paginaCorrente, $paginePubbliche)) {
+    header("Location: login-form.php");
+    exit;
+}
 ?>
