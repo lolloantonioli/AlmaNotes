d624c3a415d4caa1ddc62c010ae2ca41b5a2de5a
Rolla04 <luca.varalerolla@studio.unibo.it>
2026-01-01 12:28:12 +0100

    added my profile

--- php/template/lista-contatti.php
+++ php/template/lista-contatti.php
@@ -11,20 +11,20 @@
                     <p class="card-text">Sono uno studente di Ingegneria e Scienze Informatiche all'Università di Bologna.</p>
                 </div>
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item border-bottom border-danger-subtle"><i class="bi bi-envelope"></i> lorenzo.antonioli2@studio.unibo.it</li>
+                    <li class="list-group-item border-bottom border-danger-subtle"><i class="bi bi-envelope"></i><a class="link-danger link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover" href="mailto:lorenzo.antonioli2@studio.unibo.it"> lorenzo.antonioli2@studio.unibo.it</a></li>
                     <li class="list-group-item"><i class="bi bi-github"></i> <a class="link-danger link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover" href="https://github.com/lolloantonioli">lolloantonioli</a></li>
                 </ul>
             </div>
         </div>
         <div class="col-12 col-lg-6 d-flex justify-content-center">
             <div class="card shadow border border-danger-subtle" style="width: 18.6rem;">
-                <img src="..." class="card-img-top" alt="...">
+                <img src="img/luca.jpeg" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Luca Varale Rolla</h5>
                     <p class="card-text">Sono uno studente di Ingegneria e Scienze Informatiche all'Università di Bologna.</p>
                 </div>
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item border-bottom border-danger-subtle"><i class="bi bi-envelope"></i> luca.varalerolla@studio.unibo.it</li>
+                    <li class="list-group-item border-bottom border-danger-subtle"><i class="bi bi-envelope"></i><a class="link-danger link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover" href="mailto:luca.varalerolla@studio.unibo.it"> luca.varalerolla@studio.unibo.it</a></li>
                     <li class="list-group-item"><i class="bi bi-github"></i> <a class="link-danger link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover" href="https://github.com/Rolla04">Rolla04</a></li>
                 </ul>
             </div>
