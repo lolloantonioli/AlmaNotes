c7ffd7cc8d83a67d287084357b86a0cf53bd1b39
Lorenzo Antonioli <lorenzo.antonioli2@studio.unibo.it>
2026-01-02 19:09:37 +0100

    Added favicon and right path of css

--- php/signup-form.php
+++ php/signup-form.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AlmaNotes - Registrati</title>
+    <link rel="icon" type="image/png" href="img/logo.png"/>
     <link href="https://fonts.googleapis.com/css2?family=Merriweather+Sans:wght@300;400;700&family=Merriweather:wght@300;400;700&family=Source+Sans+3:wght@300;400;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 </head>
 
