5b7755cdccb6a32d98b31d658cab0abb3bce57d7
Lorenzo Antonioli <lorenzo.antonioli2@studio.unibo.it>
2026-01-08 11:22:08 +0100

    Changed layout signup and login

--- php/login-form.php
+++ php/login-form.php
@@ -10,38 +10,41 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link rel="stylesheet" href="/AlmaNotes/css/style.css">
 </head>
-<body class="bg-light d-flex flex-column justify-content-center align-items-center vh-100 overflow-x-hidden" style="background-image: url(img/sfondo.jpg); background-size: cover; background-position: center; background-repeat: no-repeat;">
-    <main>
-        <?php if(isset($templateParams["errorelogin"])): ?>
-            <div class="alert alert-danger">
-                <?php echo $templateParams["errorelogin"]; ?>
+<body class="bg-light d-flex flex-column justify-content-center align-items-center vh-100 overflow-x-hidden login-background">
+    <main class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-sm-10 col-md-7 col-lg-6 col-xl-4">
+                <?php if(isset($templateParams["errorelogin"])): ?>
+                    <div class="alert alert-danger">
+                        <i class="bi bi-exclamation-triangle"></i> <?php echo $templateParams["errorelogin"]; ?>
+                    </div>
+                <?php endif; ?>
+                <form action="login.php" method="POST">
+                    <div class="card bg-light bg-opacity-95 rounded-4 shadow-lg p-4 p-sm-5">
+                        <div class="text-center mb-2">
+                            <img src="img/logo.png" alt="Logo di AlmaNotes" width="70" height="70"/>
+                            <h1 class="fw-bold mb-4 red-title">Accedi</h1>
+                        </div>
+
+                        <div class="form-floating mb-2">
+                            <input type="text" class="form-control focus-ring focus-ring-danger border border-danger-subtle" id="username" name="username" placeholder="Username" autocomplete="off" required/>
+                            <label for="username">Username</label>
+                        </div>
+
+                        <div class="form-floating mb-3">
+                            <input type="password" class="form-control focus-ring focus-ring-danger border border-danger-subtle" id="password" name="password" placeholder="Password" autocomplete="off" required/>
+                            <label for="password">Password</label>
+                        </div>
+
+                        <p class="mb-3">Non sei registrato? <a href="./signup-form.php">Registrati qui</a></p>
+
+                        <div class="d-grid">
+                            <input type="submit" class="btn btn-lg fw-bold py-2 rounded-3 shadow m-0 fs-4 red-input" value="Accedi"/>
+                        </div>
+                    </div>
+                </form>
             </div>
-        <?php endif; ?>
-        <form action="login.php" method="POST">
-            <div class="bg-light bg-opacity-95 rounded-4 shadow-lg p-4 p-sm-5 mx-3" style="max-width: 450px; width: 100%;">
-                <div class="w-100 px-3" style="max-width: 400px;">
-                    <div class="text-center mb-2">
-                        <img src="img/logo.png" alt="Logo di AlmaNotes" width="70" height="70"/><h1 class="fw-bold mb-4" style="color: #BB2E29;">Accedi</h1>
-                    </div>
-                    
-                    <div class="form-floating mb-2">
-                        <input type="text" class="form-control focus-ring focus-ring-danger border border-danger-subtle" id="username" name="username" placeholder="Username" autocomplete="off" required/>
-                        <label for="username">Username</label>
-                    </div>
-                    
-                    <div class="form-floating mb-3">
-                        <input type="password" class="form-control focus-ring focus-ring-danger border border-danger-subtle" id="password" name="password" placeholder="Password" autocomplete="off" required/>
-                        <label for="password">Password</label>
-                    </div>
-    
-                    <p class="mb-3">Non sei registrato? <a href="./signup-form.php">Registrati qui</a></p>
-                    
-                    <div class="d-grid">
-                        <input type="submit" class="btn btn-lg fw-bold py-2 rounded-3 shadow m-0 fs-4" style="background-color: #BB2E29; color: white;" value="Accedi"/>
-                    </div>
-                </div>
-            </div>
-        </form>
+        </div>
     </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
